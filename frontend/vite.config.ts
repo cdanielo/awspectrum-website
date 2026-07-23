eaf775ab573a052e7628d8ba1bcd5c9157d890c9
daniel <carlosdanielocanavega@gmail.com>
2026-07-23 17:57:06 -0600

    index

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: '../',
+    assetsDir: 'assets',
+  },
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
